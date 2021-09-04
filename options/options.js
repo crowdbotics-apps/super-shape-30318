c9290170924d60a5b2a150fb92e2c7cc0a21d081
Crowdbotics <team@crowdbotics.com>
2021-09-04 23:15:47 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,6 @@
+export const globalOptions = {
+  name: "super_shape_30318Identifier",
+  url: "https://super_shape_30318Identifier.botics.co",
+  api: "https://super_shape_30318Identifier.botics.co/api/v1"
+}
+export const modulesOptions = {}
